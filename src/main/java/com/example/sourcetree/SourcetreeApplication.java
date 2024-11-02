066237d34f1db0134130fc238b28084fabe164dc
kurbanovxurshidbek <shamsun.com@gmail.com>
2024-11-02 18:14:01 +0500

    Initial commit

---
+++ src/main/java/com/example/sourcetree/SourcetreeApplication.java
@@ -0,0 +1,13 @@
+package com.example.sourcetree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourcetreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SourcetreeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sourcetree/SourcetreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sourcetree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SourcetreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
